4f2727fb1631d1f853d72ea8642ddeb5cdb82fcd
filicat <812107326@qq.com>
2024-12-26 15:00:26 +0800

    init

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common import NoSuchElementException
 from faker import Faker
+import easygui
 
 if __name__ == '__main__':
     fake = Faker('zh_CN')
@@ -22,11 +23,12 @@ if __name__ == '__main__':
     appium_server_url = 'http://localhost:4723'
     driver = webdriver.Remote(appium_server_url, options=options)
     try:
-        for i in range(15):
+        for i in range(20):
             input_elem = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="sb_form_q"]')
             input_elem.send_keys(fake.company() + ' ' + fake.name())
             submit_btn = driver.find_element(by=AppiumBy.XPATH, value='//android.widget.Button[@resource-id="sb_form_go"]')
             submit_btn.click()
             time.sleep(10)
     finally:
+        easygui.msgbox("搜索完成", title="必应搜索_移动")
         driver.quit()
